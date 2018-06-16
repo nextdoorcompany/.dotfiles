ffea40855fa0f7dbe80e24cc725604e9b26f6670
Justin <justin@nextdoorco.com>
2018-06-16 11:34:30 -0400

    Move packages to dedicated el file

--- .emacs
+++ .emacs
@@ -1,7 +1,3 @@
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
-
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
---
+++ package.el
@@ -0,0 +1,30 @@
+(require 'package)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(use-package diminish
+  :ensure t
+  )
+
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 2.0)
+  (which-key-mode 1))
+
+(use-package avy
+  :ensure t
+  :bind ("C-s" . avy-goto-char-timer))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
