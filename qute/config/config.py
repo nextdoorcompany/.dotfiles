25d26115e6f72b026be19739cc0ef6da551501cb
Justin Schechter <justin@nextdoorco.com>
2020-02-08 19:04:46 -0500

    The fonts.monospace setting has been removed.  See v1.10.0 release notes.

--- qute/config/config.py
+++ qute/config/config.py
@@ -87,7 +87,7 @@ c.colors.tabs.selected.even.bg = 'blue'
 # Default monospace fonts. Whenever "monospace" is used in a font
 # setting, it's replaced with the fonts listed here.
 # Type: Font
-c.fonts.monospace = '"Triplicate T4c", "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+# c.fonts.monospace = '"Triplicate T4c", "xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 
 # Font used in the statusbar.
 # Type: Font
