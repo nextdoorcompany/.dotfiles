a6b79e7f4612b09064156a7703ada7a6442f825a
Justin Schechter <justin@nextdoorco.com>
2021-01-30 14:37:28 -0500

    Update setting for version 2.0

--- qute/config/config.py
+++ qute/config/config.py
@@ -3,7 +3,7 @@
 #   qute://help/settings.html
 
 # Uncomment this to still load settings configured via autoconfig.yml
-# config.load_autoconfig()
+config.load_autoconfig(False)
 
 # Aliases for commands. The keys of the given dictionary are the
 # aliases, while the values are the commands they map to.
