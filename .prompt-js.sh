e13e9d1f48e4a7c132b52ca87229ffb78167a213
Justin Schechter <justin@nextdoorco.com>
2021-02-14 13:28:17 -0500

    Delete unused files

--- .prompt-js.sh
+++
@@ -1,6 +0,0 @@
-source ~/.git-prompt.sh
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWUPSTREAM="auto"
-export GIT_PS1_SHOWCOLORHINTS=1
-
-export PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\$ "'
