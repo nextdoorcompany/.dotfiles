1facbbca2edde1a9e658347221a42971b878a022
Justin Schechter <justin@nextdoorco.com>
2021-03-13 15:39:40 -0500

    Update windows script

--- windows_update_emacs.ps1
+++ windows_update_emacs.ps1
@@ -1,3 +1,4 @@
+git pull
 Copy-Item -Path config.org -Destination C:\Users\Justin\Documents\emacs26\home\.config.org
 Copy-Item -Path .emacs -Destination C:\Users\Justin\Documents\emacs26\home\.emacs
 robocopy snippets C:\Users\Justin\Documents\emacs26\home\.emacs.d\snippets /MIR
