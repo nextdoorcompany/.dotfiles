febbe76d7f7b018d1fc867609b7dd2dfeebc7ff7
Justin Schechter <justin@nextdoorco.com>
2021-08-08 14:06:38 -0400

    Add mirror update script

---
+++ update_arch_mirrors.sh
@@ -0,0 +1,5 @@
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 6 - > ~/mirrorlist
+sudo mv ~/mirrorlist /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist.old
