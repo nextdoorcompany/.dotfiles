0bd55e6e9f407fe56fa3a5739a095b6a6d42d92c
Justin Schechter <justin@nextdoorco.com>
2021-09-15 18:25:39 -0400

    Update to new home directory

--- windows_update_emacs.ps1
+++ windows_update_emacs.ps1
@@ -1,4 +1,4 @@
 git pull
-Copy-Item -Path config.org -Destination C:\Users\Justin\Documents\emacs26\home\.config.org
-Copy-Item -Path .emacs -Destination C:\Users\Justin\Documents\emacs26\home\.emacs
-robocopy snippets C:\Users\Justin\Documents\emacs26\home\.emacs.d\snippets /MIR
+Copy-Item -Path config.org -Destination C:\Users\Justin\Documents\emacs27\.config.org
+Copy-Item -Path .emacs -Destination C:\Users\Justin\Documents\emacs27\.emacs
+robocopy snippets C:\Users\Justin\Documents\emacs27\.emacs.d\snippets /MIR
