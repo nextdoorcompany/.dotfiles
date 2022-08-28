a312c25d95a237f587c345054fd71bc7dabf7061
Justin Schechter <justin@nextdoorco.com>
2022-08-28 06:57:38 -0400

    Remove snippets portion of Windows update script

--- windows_update_emacs.ps1
+++ windows_update_emacs.ps1
@@ -1,4 +1,3 @@
 git pull
 Copy-Item -Path config.org -Destination C:\Users\Justin\Documents\emacs\.config.org
 Copy-Item -Path .emacs -Destination C:\Users\Justin\Documents\emacs\.emacs
-robocopy snippets C:\Users\Justin\Documents\emacs\.emacs.d\snippets /MIR
