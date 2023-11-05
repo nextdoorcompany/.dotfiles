2e1d9f31ec6a7869634b7b7232b785ff398293b3
Justin Schechter <justin@nextdoorco.com>
2023-11-05 06:53:12 -0500

    Include injected

--- update_python_tools.sh
+++ update_python_tools.sh
@@ -1,4 +1,4 @@
 ~/venv/pipx3.12/bin/python -m pip install -U pip pipx
-pipx upgrade-all
+pipx upgrade-all --include-injected
 ~/venv/batteries3.12/bin/pip-compile --generate-hashes --strip-extras --upgrade ~/venv/batteries3.12/requirements.in
 ~/venv/batteries3.12/bin/pip-sync ~/venv/batteries3.12/requirements.txt
