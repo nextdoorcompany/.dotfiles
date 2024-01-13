2fcf753344eef778fa7a6771cfbbf116a70ddf77
Justin Schechter <justin@nextdoorco.com>
2024-01-13 06:01:37 -0500

    Update pip

--- update_python_tools.sh
+++ update_python_tools.sh
@@ -1,4 +1,5 @@
 ~/venv/pipx3.12/bin/python -m pip install -U pip pipx
 pipx upgrade-all --include-injected
+~/venv/batteries3.12/bin/python -m pip install -U pip
 ~/venv/batteries3.12/bin/pip-compile --generate-hashes --strip-extras --upgrade ~/venv/batteries3.12/requirements.in
 ~/venv/batteries3.12/bin/pip-sync ~/venv/batteries3.12/requirements.txt
